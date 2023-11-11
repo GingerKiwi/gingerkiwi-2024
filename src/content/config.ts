aa5ea51f9683a112ab7b4f5e0c7b3d8593c0a1da
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-11-11 11:26:22 -0500

    applied fix for ts error in [page].astro from Creatures.dev discord - thx Lazar and Dreyfus

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 
 const blogCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        tags: z.array(z.string()),
-        date: z.date(),
-    })
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    date: z.date(),
+    description: z.string().optional(),
+  }),
 });
 
 export const collections = {
-    blog: blogCollection,
+  blog: blogCollection,
 };
