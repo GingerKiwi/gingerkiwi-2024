e30782c9c0cb31437dd5e590c18d609473b049b2
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-11-11 17:57:26 -0500

    run npx astro add react and say Y to all options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gingerkiwi.dev',
   integrations: [tailwind(), sitemap({
-    filter: (page) => !/https:\/\/gingerkiwi\.dev\/blog\/[0-9]+/.test(page),
-    customPages: [
-      'https://www.gingerkiwi.dev/linkedin'
-    ],
+    filter: page => !/https:\/\/gingerkiwi\.dev\/blog\/[0-9]+/.test(page),
+    customPages: ['https://www.gingerkiwi.dev/linkedin'],
     changefreq: 'weekly',
     lastmod: new Date(),
     priority: 0.85,
-    serialize: (item) => {
-      if (item.url.at(-1) === '/'){
+    serialize: item => {
+      if (item.url.at(-1) === '/') {
         item.url = item.url.slice(0, -1);
       }
       return item;
@@ -28,5 +27,5 @@ export default defineConfig({
         fr: 'fr-FR'
       }
     }
-  })]
+  }), react()]
 });
