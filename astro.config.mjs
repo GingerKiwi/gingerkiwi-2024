2a7cead7e8aae4b902c0ba52e5cfe42b252cd447
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-11-13 10:28:58 -0500

    run npx add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gingerkiwi.dev',
@@ -25,5 +27,9 @@ export default defineConfig({
         fr: 'fr-FR'
       }
     }
-  }), ]
+  })],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
