2987b5351665ec9a3b19a2e565c7a7b1f397f24f
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-11-14 13:43:18 -0500

    fix typos in index.ts - line 8 , to . line 11 scr to src

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,9 +5,9 @@ import {
 } from 'drizzle-orm/better-sqlite3';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
-const sqlite = new Database(process.env.DATABASE_URL || ',/local.db');
+const sqlite = new Database(process.env.DATABASE_URL || './local.db');
 
 export const db:BetterSQLite3Database = drizzle(sqlite, { logger: true });
-migrate(db, { migrationsFolder: 'scr/db/migrations'});
+migrate(db, { migrationsFolder: 'src/db/migrations'});
 
 export { likes } from './schema';
