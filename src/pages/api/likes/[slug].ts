98dba109325ca49aa8e0126b13f93b48350e756f
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-11-24 13:48:58 -0500

    finish creating api endpoints. fix multiple typos like -> likes . It works

--- src/pages/api/likes/[slug].ts
+++ src/pages/api/likes/[slug].ts
@@ -5,25 +5,73 @@ import { eq } from 'drizzle-orm';
 export const prerender = false;
 
 export async function GET(context: APIContext) {
-    const { slug } = context.params;
+  const { slug } = context.params;
 
-    if (!slug) {
-        return new Response ('Bad Request', { status: 400 });
-        }   
+  if (!slug) {
+    return new Response('Bad Request', { status: 400 });
+  } 
 
-        const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
+const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
         
-        if(!entry) {
-            return new Response('Not Found', { status: 404 });
-        }
-
-    return new Response(JSON.stringify(entry), {
+         if (!entry) {
+    return new Response(
+      JSON.stringify({
+        slug,
+        likes: 0,
+      }),
+                {
         status: 200,
         headers: {
-            "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-});
-
+      }
+    );
+  }
+        
+    return new Response(JSON.stringify(entry), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
 
-export async function POST() {}
+export async function POST(context: APIContext) {
+  const { slug } = context.params;
+
+  if (!slug) {
+    return new Response('Bad Request', { status: 400 });
+  }
+
+  const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
+
+    if (!entry) {
+    // Create new entry
+    const inserted = db
+      .insert(likes)
+      .values({ slug, likes: 1 })
+      .returning()
+      .get();
+
+    return new Response(JSON.stringify(inserted), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+        const updated = db
+    .update(likes)
+    .set({ likes: (entry.likes ?? 0) + 1 })
+    .where(eq(likes.slug, slug))
+    .returning()
+    .get();
+
+  return new Response(JSON.stringify(updated), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
