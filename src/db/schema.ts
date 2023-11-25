7739bf062dfbf0c4c9d9a1f64dc268aec1f89ed3
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-11-25 10:37:21 -0500

    add .primaryKey() to src/db/schema.ts on line 4 -> was slug: text('slug',

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';;
 
 export const likes = sqliteTable('likes', {
-    slug: text('slug'),
+    slug: text('slug').primaryKey(),
     likes: integer('likes').default(0)
 });
