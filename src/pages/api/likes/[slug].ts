9941cda52b5bd2695e17a5bf5a7c3f047ef94285
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-11-25 09:44:07 -0500

    debug src/pages/api/likes/[slug].ts by copy paste lz repo code. can't find typo. worth a try

--- src/pages/api/likes/[slug].ts
+++ src/pages/api/likes/[slug].ts
@@ -1,5 +1,5 @@
-import type { APIContext } from "astro";
-import { db, likes } from "../../../db";
+import type { APIContext } from 'astro';
+import { db, likes } from '../../../db';
 import { eq } from 'drizzle-orm';
 
 export const prerender = false;
@@ -9,17 +9,17 @@ export async function GET(context: APIContext) {
 
   if (!slug) {
     return new Response('Bad Request', { status: 400 });
-  } 
+  }
 
-const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
-        
-         if (!entry) {
+  const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
+
+  if (!entry) {
     return new Response(
       JSON.stringify({
         slug,
         likes: 0,
       }),
-                {
+      {
         status: 200,
         headers: {
           'Content-Type': 'application/json',
@@ -27,8 +27,8 @@ const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
       }
     );
   }
-        
-    return new Response(JSON.stringify(entry), {
+
+  return new Response(JSON.stringify(entry), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
@@ -45,7 +45,7 @@ export async function POST(context: APIContext) {
 
   const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
 
-    if (!entry) {
+  if (!entry) {
     // Create new entry
     const inserted = db
       .insert(likes)
@@ -61,7 +61,7 @@ export async function POST(context: APIContext) {
     });
   }
 
-        const updated = db
+  const updated = db
     .update(likes)
     .set({ likes: (entry.likes ?? 0) + 1 })
     .where(eq(likes.slug, slug))
@@ -75,3 +75,82 @@ export async function POST(context: APIContext) {
     },
   });
 }
+
+
+// import type { APIContext } from "astro";
+// import { db, likes } from "../../../db";
+// import { eq } from 'drizzle-orm';
+
+// export const prerender = false;
+
+// export async function GET(context: APIContext) {
+//   const { slug } = context.params;
+
+//   if (!slug) {
+//     return new Response('Bad Request', { status: 400 });
+//   } 
+
+// const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
+        
+//          if (!entry) {
+//     return new Response(
+//       JSON.stringify({
+//         slug,
+//         likes: 0,
+//       }),
+//                 {
+//         status: 200,
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       }
+//     );
+//   }
+        
+//     return new Response(JSON.stringify(entry), {
+//     status: 200,
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+// }
+
+// export async function POST(context: APIContext) {
+//   const { slug } = context.params;
+
+//   if (!slug) {
+//     return new Response('Bad Request', { status: 400 });
+//   }
+
+//   const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
+
+//     if (!entry) {
+//     // Create new entry
+//     const inserted = db
+//       .insert(likes)
+//       .values({ slug, likes: 1 })
+//       .returning()
+//       .get();
+
+//     return new Response(JSON.stringify(inserted), {
+//       status: 200,
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
+//   }
+
+//         const updated = db
+//     .update(likes)
+//     .set({ likes: (entry.likes ?? 0) + 1 })
+//     .where(eq(likes.slug, slug))
+//     .returning()
+//     .get();
+
+//   return new Response(JSON.stringify(updated), {
+//     status: 200,
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+// }
