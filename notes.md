2b5defcf9da76843613693c5cdb7adb62388edbc
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-12-05 18:22:07 -0500

    responsive header works

---
+++ notes.md
@@ -0,0 +1,7 @@
+# To Dos
+
+## 2023-12-04: Monday, December 04, 2023
+
+- [ ] figure out why LinkNav works for desktop but not tablet breakpoint
+- [ ] figure out why Header centers on desktop (not desired) but not on tablet (desired)
+- [ ] Fix extra menu showing in mobile - again!
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		// screens: {
+		// 	'tablet': '640px',
+		// 	// => @media (min-width: 640px) { ... }
+	  
+		// 	'laptop': '1024px',
+		// 	// => @media (min-width: 1024px) { ... }
+	  
+		// 	'desktop': '1280px',
+		// 	// => @media (min-width: 1280px) { ... }
+		//   },
 		extend: {},
 	},
 	plugins: [
