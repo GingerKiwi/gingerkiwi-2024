5da5c6f0b6cbf9e63cadb4b8f35039345da60ea5
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-12-06 12:22:14 -0500

    mobile nav design tweaks. Not done but works

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,9 @@ import { z, defineCollection } from 'astro:content';
 const blogCollection = defineCollection({
   type: 'content',
   schema: ({ image }) => z.object({
+    date: z.date(),
     title: z.string(),
     tags: z.array(z.string()),
-    date: z.date(),
     description: z.string().optional(),
     cover: image().refine((img) => img.width >= 1080, {
       message: "Cover image must be at least 1080 pixels wide!",
